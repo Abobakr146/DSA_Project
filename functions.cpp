d99a758a72ac7a4e2c170c95ed4f66dfd4b528b5
Abdo11111999 <abdelrahmanashour098@gmail.com>
2025-12-08 16:17:13 +0200

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -181,7 +181,40 @@ string json(const string& xml) {
 }
 
 string mini(const string& xml) {
-    return "";
+    string output;
+    bool inTag = false;   // true when inside <...>
+    bool inText = false;  // true when between > and next <
+
+    for (char c : xml) {
+        if (c == '<') {
+            inTag = true;
+            inText = false;
+            output += c;
+        }
+        else if (c == '>') {
+            inTag = false;
+            inText = true;
+            output += c;
+        }
+        else {
+            if (inTag) {
+                // Inside a tag name or attribute: remove ALL whitespace
+                if (c != ' ' && c != '\n' && c != '\t' && c != '\r')
+                    output += c;
+            }
+            else if (inText) {
+                // Inside text content: KEEP spaces between words, remove formatting whitespace
+                if (c == '\n' || c == '\t' || c == '\r')
+                    continue;
+
+                // Remove multiple spaces; keep single space
+                if (!(c == ' ' && (output.back() == ' ' || output.back() == '>')))
+                    output += c;
+            }
+        }
+    }
+
+    return output;
 }
 
 string compress(const string& xml) {
