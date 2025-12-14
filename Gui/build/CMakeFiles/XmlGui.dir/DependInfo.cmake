f6fa8fe4883ebc2f6a431b2a5558c24a23c80d1f
Ali Mahmoud <alimahmoud235@outlook.com>
2025-12-14 19:22:44 +0200

    Gui v2

---
+++ Gui/build/CMakeFiles/XmlGui.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "" "XmlGui_autogen/timestamp" "custom" "XmlGui_autogen/deps"
+  "C:/Users/alima/OneDrive/Desktop/DSA_Project/Gui/build/XmlGui_autogen/mocs_compilation.cpp" "CMakeFiles/XmlGui.dir/XmlGui_autogen/mocs_compilation.cpp.obj" "gcc" "CMakeFiles/XmlGui.dir/XmlGui_autogen/mocs_compilation.cpp.obj.d"
+  "C:/Users/alima/OneDrive/Desktop/DSA_Project/Gui/functions.cpp" "CMakeFiles/XmlGui.dir/functions.cpp.obj" "gcc" "CMakeFiles/XmlGui.dir/functions.cpp.obj.d"
+  "C:/Users/alima/OneDrive/Desktop/DSA_Project/Gui/main.cpp" "CMakeFiles/XmlGui.dir/main.cpp.obj" "gcc" "CMakeFiles/XmlGui.dir/main.cpp.obj.d"
+  "C:/Users/alima/OneDrive/Desktop/DSA_Project/Gui/mainwindow.cpp" "CMakeFiles/XmlGui.dir/mainwindow.cpp.obj" "gcc" "CMakeFiles/XmlGui.dir/mainwindow.cpp.obj.d"
+  "C:/Users/alima/OneDrive/Desktop/DSA_Project/Gui/utils.cpp" "CMakeFiles/XmlGui.dir/utils.cpp.obj" "gcc" "CMakeFiles/XmlGui.dir/utils.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
